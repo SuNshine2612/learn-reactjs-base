07e73ef539adb09f1272015f56b2030927ba86d6
SuNshine2612 <anhminh2612@gmail.com>
2023-03-24 11:22:18 +0700

    Test deploy 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="app">Learn ReactJS - SuNshine 2612</div>;
+  return <div className="app">Learn ReactJS - SuNshine 2612 - 24/03/2023</div>;
 }
 
 export default App;
